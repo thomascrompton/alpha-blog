17da0603092895af9ce2d5f27d164539a823cbfb
thomascrompton <tcrom2000@gmail.com>
2019-01-18 13:54:28 -0500

    Add admin requirement to create categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
     
-    test "get new category forma and create category" do
+    test "get new category form and create category" do
         get new_category_path
         assert_template 'categories/new'
         assert_difference 'Category.count', 1 do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,11 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
+    def sign_in_as(user, password)
+        post login_path, params: { session: { email: user.email, password: password } }
+    end
 end
